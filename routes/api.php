fe1818939fa364e5a8248591f4cc129a9fc68d62
Diego González <diegonurr@hotmail.com>
2023-12-03 04:35:53 -0300

    actualizado api con nuevas rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(CustomerController::class)->group(function () {
+    Route::get('/customers','index');
+    Route::get('/orders/{id}','show');
+});
+
+Route::controller(EventController::class)->group(function () {
+    Route::get('/event/{id}','show');
+    Route::get('/events','index');
+});
+
+Route::controller(PurchaseController::class)->group(function () {
+    Route::post('/purchase','store');
+
 });
